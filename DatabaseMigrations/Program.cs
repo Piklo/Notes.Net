d07364c083e8282e447ce914a59209c9db2ad8a8
Piklo <16057987+Piklo@users.noreply.github.com>
2023-08-27 18:15:29 +0200

    renamed secret Notes:ConnectionString to ConnectionStrings:Notes

--- DatabaseMigrations/Program.cs
+++ DatabaseMigrations/Program.cs
@@ -6,8 +6,7 @@ var builder = new ConfigurationBuilder();
 builder.AddUserSecrets<Program>();
 var config = builder.Build();
 
-var connectionString = config.GetSection("Notes:ConnectionString").Value;
-
+var connectionString = config.GetConnectionString("Notes");
 
 var upgrader = DeployChanges.To.SQLiteDatabase(connectionString)
     .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
--- Readme.md
+++ Readme.md
@@ -1,2 +1,2 @@
 add connection string to both DatabaseMigrations and Notes-MinimalApi projects
-dotnet user-secrets set "Notes:ConnectionString" "Data Source=Path\To\The\database.db"
+dotnet user-secrets set "ConnectionStrings:Notes" "Data Source=Path\To\The\database.db"
