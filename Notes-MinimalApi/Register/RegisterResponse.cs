efcde385441aebe9d0fd932e5384ed20d2c9508f
Piklo <16057987+Piklo@users.noreply.github.com>
2023-08-28 18:08:30 +0200

    added JsonStringEnumConverter to RegisterResponse.Status

--- Notes-MinimalApi/Register/RegisterResponse.cs
+++ Notes-MinimalApi/Register/RegisterResponse.cs
@@ -1,6 +1,9 @@
-﻿namespace Notes_MinimalApi.Register;
+﻿using System.Text.Json.Serialization;
+
+namespace Notes_MinimalApi.Register;
 
 internal sealed class RegisterResponse
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public required RegisterStatus Status { get; init; }
 }
