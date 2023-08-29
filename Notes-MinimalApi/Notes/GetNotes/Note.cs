c7b5f47f0d7722f8d2b5457a4de1be315fe32c6b
Piklo <16057987+Piklo@users.noreply.github.com>
2023-08-29 13:36:14 +0200

    deleted Note class

--- Notes-MinimalApi/Notes/GetNotes/Note.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Notes_MinimalApi.Notes.GetNotes;
-
-public sealed class Note
-{
-    public required Guid Id { get; init; }
-    public required Guid UserId { get; init; }
-    public required string Value { get; init; }
-}
