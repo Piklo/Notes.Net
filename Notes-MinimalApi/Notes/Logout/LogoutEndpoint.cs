43e092dabb76f7b27193d7914aaa6bdbe576dd85
Piklo <16057987+Piklo@users.noreply.github.com>
2023-09-03 19:45:01 +0200

    added logout endpoint

---
+++ Notes-MinimalApi/Notes/Logout/LogoutEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Notes_MinimalApi.Notes.Logout;
+
+internal static class LogoutEndpoint
+{
+    public static void MapLogoutEndpoint(this WebApplication app)
+    {
+        app.MapPost("/logout", HandleAsync);
+    }
+
+    private static async Task HandleAsync(HttpContext context)
+    {
+        await context.SignOutAsync();
+    }
+}
--- Notes-MinimalApi/Program.cs
+++ Notes-MinimalApi/Program.cs
@@ -3,6 +3,7 @@ using Notes_MinimalApi.Database;
 using Notes_MinimalApi.Notes.AddNote;
 using Notes_MinimalApi.Notes.GetNote;
 using Notes_MinimalApi.Notes.GetNotes;
+using Notes_MinimalApi.Notes.Logout;
 using Notes_MinimalApi.Notes.RemoveNote;
 using Notes_MinimalApi.Notes.UpdateNote;
 using Notes_MinimalApi.User.Login;
@@ -57,6 +58,7 @@ app.UseCors();
 //app.UseHttpsRedirection();
 
 app.MapLoginEndpoint();
+app.MapLogoutEndpoint();
 app.MapRegisterEndpoint();
 app.MapAddNoteEndpoint();
 app.MapGetNotesEndpoint();
